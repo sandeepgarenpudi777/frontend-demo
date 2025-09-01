3ea2d9fe92ba14a2b5ef32ad876ec205d042a8a6
Sandeep Garnepudi <sandeepfullstackjava1@gmail.com>
2025-09-01 16:01:48 +0530

    changed the backend url

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API ='http://localhost:8080/api/projects';
+const API = import.meta.VITE_API_URL ||'http://localhost:8080/api/projects';
 
 export const getAll = () => axios.get(API);
 
